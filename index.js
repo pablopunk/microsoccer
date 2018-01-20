83a851432e824bf8d06c5ac026b3525718b6ea1c
Pablo Varela <pablovarela182@gmail.com>
2018-01-20 19:35:09 +0100

    Tunnel timezone to request

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const {send} = require('micro')
 const getMatches = require('livesoccertv-parser')
+const requestIp = require('request-ip')
+const isIp = require('is-ip')
+const pify = require('pify')
+const satelize = pify(require('satelize').satelize)
 
 const getFolderFromUrl = (url, index) => url.split('/')[index]
 const getCountryFromUrl = url => getFolderFromUrl(url, 1)
@@ -10,6 +14,7 @@ const cacheLife = 1000 * 60 * 24 // 1 day
 
 module.exports = async (req, res) => {
   const [country, team] = [getCountryFromUrl(req.url), getTeamFromUrl(req.url)]
+
   let headersSent = false
 
   if (!country || !team) {
@@ -17,14 +22,21 @@ module.exports = async (req, res) => {
     return
   }
 
+  const ip = requestIp.getClientIp(req).split(':').pop()
+  let timezone = 'Europe/Madrid'
+
+  if (isIp.v4(ip)) {
+    timezone = (await satelize({ ip })).timezone
+  }
+
   if (!cache[country] || !cache[country][team]) {
-    await populateCache(country, team)
+    await populateCache(country, team, timezone)
       .catch(err => {
         console.log(err.message)
         send(res, 404, 'Not found')
         headersSent = true
       })
-    setInterval(() => populateCache(country, team), cacheLife)
+    setInterval(() => populateCache(country, team, timezone), cacheLife)
   }
 
   if (!headersSent) {
@@ -32,8 +44,8 @@ module.exports = async (req, res) => {
   }
 }
 
-async function populateCache(country, team) {
+async function populateCache (country, team, timezone) {
   console.log(`Populating ${country}/${team}`)
   cache[country] = {}
-  cache[country][team] = await getMatches(country, team)
+  cache[country][team] = await getMatches(country, team, {timezone})
 }
